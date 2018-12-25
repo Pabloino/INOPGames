37fe0c2734eccb26bb51e31f0a72fab1f0b1c154
Pablo <pablo-ino@hotmail.com>
2018-12-25 19:17:28 -0300

    Fixed problems

---
+++ data/lang/lang.js
@@ -0,0 +1,24 @@
+var lang_text = '{' + 
+' "language" : [' +
+		'{' +
+			' "home" : "HOME", ' + 
+			' "games" : "GAMES", ' + 
+			' "news" : "NEWS", ' + 
+			' "about" : "ABOUT", ' + 
+			' "lang" : "SP", ' + 
+			' "langMobile" : "SPANISH VERSION", ' + 
+			' "langTitle" : "Spanish version" ' +
+		'},' +
+		'{' +
+			' "home" : "INICIO", ' +
+			' "games" : "JUEGOS", ' + 
+			' "news" : "NOTICIAS", ' + 
+			' "about" : "SOBRE MÍ", ' + 
+			' "lang" : "EN", ' + 
+			' "langMobile" : "ENGLISH VERSION", ' + 
+			' "langTitle" : "English version" ' +
+		'}' +  
+	']' +
+'}';
+
+var obj = JSON.parse(lang_text);
